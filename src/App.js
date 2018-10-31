3a752bfb76c26360808399a7aeef185027a4bb51
LehmanBrother <chrislehman1991@gmail.com>
2018-10-31 12:08:09 -0500

    Part 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+const endPoint = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson'
 
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      earthquakes: ''
+    }
+  }
+
+  getEarthquakes = async () => {
+    try {
+      const earthquakes = await fetch(endPoint);
+      const earthquakesJson = await earthquakes.json();
+      return earthquakesJson;
+    } catch(err) {
+      return err
+    }
+  }
+
+  componentDidMount(){
+    this.getEarthquakes().then((earthquakes) => {
+      console.log(earthquakes, ' this is data');
+      this.setState({earthquakes: earthquakes});
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
 
   render() {
     return (
